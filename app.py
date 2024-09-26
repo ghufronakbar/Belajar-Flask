4a6b597625bb8e528e27dbda05fcbff158dcaf29
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-26 15:16:35 +0700

    inti repo

---
+++ app.py
@@ -0,0 +1,30 @@
+# CREATE FLASK APP
+
+## Install virtualenv globally
+### pip install virtualenv
+
+## Create virtualenv
+### virtualenv venv
+
+## Activate virtualenv
+### venv/Scripts/activate
+
+## Install Flask
+### pip install flask
+
+## Run Flask
+### flask run
+
+## Source Code:
+from json import JSONEncoder, JSONDecoder
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
